30cb7726a827a76e098fbe17a5c00adde52c715f
andrew urry <andy@andy-HP-Pavilion-g6-Notebook-PC.home>
2018-03-30 23:12:22 +0100

    Updated solutions

--- problem_3.py
+++ problem_3.py
@@ -1,41 +1,39 @@
-# is_prime = True
-# number = 3
-# prime_numbers = [2]
-# maximum = 600851475143
-
-# while number*2 < maximum:
-    
-#     for prime_number in prime_numbers[:]:
-#         if number % prime_number == 0:
-#             is_prime = False
-#             break
-    
-#     if is_prime:
-#         prime_numbers.append(number)
-
-#     is_prime = True
-#     number += 1
-
-# for prime_number in reversed(prime_numbers):
-#     if maximum % prime_number == 0:
-#         print(prime_numbers)       
-#         break 
-
 import math
-# Fermat's Factorization
-# N = a^2 + b^2
 
-value_to_find = 600851475143
-a = math.ceil(math.sqrt(value_to_find))
-b = 0
+def is_prime_number(number, prime_numbers):
 
-while True:
-    b = abs((math.pow(a,2) - value_to_find))
+    is_prime = True
 
-    if (math.sqrt(b)).is_integer():
-        break
+    for prime_number in prime_numbers:
+
+        if number % prime_number == 0:
+            is_prime = False
+            return 1
+
+        if number < pow(prime_number, 2) :
+            if(is_prime):
+                prime_numbers += [number]
+                return number
+
+    return 1
+
+def find_factor(maximum):
     
-    a += 1
+    prime_numbers = [2]
+    current_number = 3
 
-print(b)
+    while maximum != 1:
+        number = is_prime_number(current_number, prime_numbers)
+        current_number += 1
 
+        if number != 1:
+            if maximum % number == 0:
+                print(number)
+                maximum /= number
+        
+def main ():
+    maximum = 600851475143
+    find_factor(maximum)
+
+if __name__ == "__main__":
+    main()
